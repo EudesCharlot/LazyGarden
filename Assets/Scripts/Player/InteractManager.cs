28ed0ebe9979db541a74da1fac4f225e012bd699
Eudes <fufudidou@gmail.com>
2025-10-15 18:03:42 +0200

    ajouit d'une sauveggarde et d'un début d'inventaire

---
+++ Assets/Scripts/Player/InteractManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class InteractManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        Debug.Log("Téma la taille du pipi");
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,9 +7,9 @@ public class PlayerController : MonoBehaviour
     public InputActionReference moveActionRef;
     public float speed = 1;
     public float rotationSpeed = 1;
-    void Awake()
+    void Start()
     {
-        
+        transform.position = JsonUtility.FromJson<Vector3>(PlayerPrefs.GetString("dronePos"));
     }
     
     void Update()
@@ -17,6 +17,10 @@ public class PlayerController : MonoBehaviour
         Vector2 stickDirection = moveActionRef.action.ReadValue<Vector2>();
         transform.Translate(new(stickDirection.x * speed * Time.deltaTime,0,stickDirection.y * speed * Time.deltaTime),Space.Self);
         transform.Rotate(Vector3.up, stickDirection.x * rotationSpeed * Time.deltaTime);
+        
+        string JSONTransform = JsonUtility.ToJson(transform.position);
+        PlayerPrefs.SetString("dronePos", JSONTransform);
+        PlayerPrefs.Save();
     }
     
 
