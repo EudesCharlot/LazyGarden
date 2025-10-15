4398d73fcb8f856cfc9a440ac59bdd3f02441031
Eudes <fufudidou@gmail.com>
2025-10-15 15:45:10 +0200

    Ajout du prefab herbe et graine

--- Assets/Scripts/Player/BoundaryLimiter.cs
+++ Assets/Scripts/Player/BoundaryLimiter.cs
@@ -5,21 +5,11 @@ public class BoundaryLimiter : MonoBehaviour
     public Vector2 Xlimits;
     public Vector2 Zlimits;
 
-    private Rigidbody rb;
-
-    void Awake()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
-
     void Update()
     {
-        Vector3 pos = rb.position;
-
-        float clampedX = Mathf.Clamp(pos.x, Xlimits.x, Xlimits.y);
-        float clampedZ = Mathf.Clamp(pos.z, Zlimits.x, Zlimits.y);
-
-        Vector3 clampedPos = new Vector3(clampedX, pos.y, clampedZ);
-        rb.MovePosition(clampedPos);
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Clamp(pos.x, Xlimits.x, Xlimits.y);
+        pos.z = Mathf.Clamp(pos.z, Zlimits.x, Zlimits.y);
+        transform.position = pos;
     }
 }
